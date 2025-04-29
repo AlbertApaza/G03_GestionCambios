22ea95e4b564f29bc2a8f683068e5c2bd2a6a0dd
erick <erichuracutipa@upt.pe>
2025-04-29 01:08:05 -0500

    asignacion de equipo

---
+++ AppGCS/Controllers/PullController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppGCS.Controllers
+{
+    public class PullController : Controller
+    {
+        // GET: Pull
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
